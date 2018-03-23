a66bf48da52bf5a8ffc2d94f5b1a696d9ec7e2be
HaymakerJones <js388111@gmail.com>
2018-03-23 16:15:42 -0400

    addValue in DataService works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DATA_PROVIDER } from './services/data.service';
+import { DemoComponent } from './demo/demo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Inject, } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { Observable } from 'rxjs/Rx';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css'],
+})
+export class DemoComponent implements OnInit {
+
+  data: Observable<number[]>;
+
+  constructor(@Inject(DataService) private ds) { }
+
+  ngOnInit() {
+    this.data = this.ds.values;
+  }
+
+  addValue(f: any) {
+
+    this.ds.addValue(f.value);
+  }
+
+}
