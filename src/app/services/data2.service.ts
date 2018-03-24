43bc2d6f0137dbab6f0f7874d34d0019553db0a9
HaymakerJones <js388111@gmail.com>
2018-03-24 09:08:16 -0400

    data 2 service finished

--- src/app/services/data2.service.ts
+++ src/app/services/data2.service.ts
@@ -2,13 +2,39 @@ import { Injectable } from '@angular/core';
 import { Person } from '../models/person.model';
 import { Observable, Subject } from 'rxjs/Rx';
 
-interface
+interface PersonOperation extends Function {
+    (persons: Person[]): Person[];
+}
 
+const initialPersons: Person[] = [];
 
 @Injectable()
 export class Data2Service {
 
-    constructor() { }
+    people: Observable<Person[]>;
+    updates: Subject<any> = new Subject<any>();
+
+    create: Subject<Person> = new Subject<Person>();
+
+    constructor() {
+        this.people =
+            this.updates
+                .scan((persons: Person[], operation: PersonOperation) => { return operation(persons); }, initialPersons)
+                .publishReplay(1)
+                .refCount();
+
+        this.create.map(
+            (person: Person): PersonOperation => {
+                return (persons: Person[]) => {
+                    return persons.concat(person);
+                }
+            }
+        ).subscribe(this.updates);
+    }
+
+    addPerson(person: Person): void {
+        this.create.next(person);
+    }
 }
 
 export const DATA_2_PROVIDER: Array<any> = [
